c7e6143b0ebcbab080fe27abbe105ff22d74b423
Pranay Kumar Reddy Goli <golipranayreddy@msitprogram.net>
2020-04-24 22:25:33 +0530

    final_changes

--- create.py
+++ create.py
@@ -7,7 +7,7 @@ from models import *
 # Settings
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://shbhdlrudoukbc:56b95ae93382d47bfe6f63214cd8fdd5bb7353e73867d0c6f1f09d5a390fc60a@ec2-54-165-36-134.compute-1.amazonaws.com:5432/d4e8h9gsq3giaq"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
